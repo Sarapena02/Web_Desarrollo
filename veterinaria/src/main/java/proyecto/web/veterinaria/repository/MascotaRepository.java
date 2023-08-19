ca4bdd985215f5276b56f8d45d34e0c5bdb9c4e7
Sarapena02 <sarapenagomez02@gmail.com>
2023-08-19 18:39:34 -0500

    Agregue foto en tabla de pacientes y estilos

--- veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
+++ veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
@@ -14,19 +14,12 @@ public class MascotaRepository {
     private Map<Integer, Mascota> mascotas = new HashMap<>();
 
     public MascotaRepository(){
-<<<<<<< Updated upstream
-        mascotas.put(1, new Mascota("Jack", "Siames", 2, "NA", "Activo","/jack1.jpeg"));
-        mascotas.put(2, new Mascota("Sasha", "Siames", 3, "Hipotrofia", "Activo",""));
-        mascotas.put(3, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo",""));
-        mascotas.put(4, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo",""));
-        mascotas.put(5, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo",""));
-=======
+
         mascotas.put(1, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
         mascotas.put(2, new Mascota("Sasha", "Siames", 3, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
         mascotas.put(3, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
         mascotas.put(4, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
         mascotas.put(5, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
->>>>>>> Stashed changes
     }
 
     public Mascota findById(Integer id){
