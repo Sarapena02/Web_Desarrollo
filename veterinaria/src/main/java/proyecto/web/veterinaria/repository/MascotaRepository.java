c6086ba60d37e270c50f5d3cfd3f5d5e7c60b8ee
clarck2547 <juandavidh76@gmail.com>
2023-08-23 09:19:06 -0500

    CRUD mascota

--- veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
+++ veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
@@ -15,11 +15,11 @@ public class MascotaRepository {
 
     public MascotaRepository(){
 
-        mascotas.put(1, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "/jack1.jpeg"));
-        mascotas.put(2, new Mascota("Sasha", "Siames", 3, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
-        mascotas.put(3, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
-        mascotas.put(4, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
-        mascotas.put(5, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg"));
+        mascotas.put(1, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "/jack1.jpeg",1));
+        mascotas.put(2, new Mascota("Sasha", "Siames", 3, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg",2));
+        mascotas.put(3, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg",3));
+        mascotas.put(4, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg",4));
+        mascotas.put(5, new Mascota("Jack", "Siames", 2, "Hipotrofia", "Activo", "https://estaticos-cdn.prensaiberica.es/clip/ce847ef4-8930-42e0-9c1d-9929902d3820_16-9-discover-aspect-ratio_default_0.jpg",5));
     }
 
     public Mascota findById(Integer id){
