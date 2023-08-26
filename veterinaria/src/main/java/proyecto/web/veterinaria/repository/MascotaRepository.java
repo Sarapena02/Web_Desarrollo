a2ce67390fedbc7f3c2c3eee74750af5f4d67baa
Saya <fl.daniela@javeriana.edu.co>
2023-08-25 22:03:35 -0500

    Arreglo de carpetas, estilo y rutas
    :)

--- veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
+++ veterinaria/src/main/java/proyecto/web/veterinaria/repository/MascotaRepository.java
@@ -14,13 +14,13 @@ public class MascotaRepository {
     private Map<Integer, Mascota> mascotas = new HashMap<>();
 
     public MascotaRepository(){
-
-        mascotas.put(1, new Mascota("Jack", "Siames", 2, "NA", "Activo", "jack1.jpeg",1));
-        mascotas.put(2, new Mascota("Sasha", "Siames", 3, "NA", "Activo", "gato1.jpg",2));
-        mascotas.put(3, new Mascota("Paco", "Siames", 2, "NA", "Activo", "gato2.jpg",3));
-        mascotas.put(4, new Mascota("Simon", "Siames", 2, "NA", "Activo", "gato3.jpg",4));
-        mascotas.put(5, new Mascota("Luna", "Siames", 2, "NA", "Activo", "gato4.jpg",5));
-        mascotas.put(6, new Mascota("Manolo", "Siames", 2, "NA", "Activo", "gato4.jpg",6));
+        mascotas.put(1, new Mascota("Jack", "Siames", 2, "NA", "Activo", "/Imagenes/jack1.jpeg",1));
+        mascotas.put(2, new Mascota("Sasha", "Siames", 3, "NA", "Activo", "/Imagenes/gato1.jpg",2));
+        mascotas.put(3, new Mascota("Paco", "Siames", 2, "NA", "Activo", "/Imagenes/gato2.jpg",3));
+        mascotas.put(4, new Mascota("Simon", "Siames", 2, "NA", "Activo", "/Imagenes/gato3.jpg",4));
+        mascotas.put(5, new Mascota("Luna", "Siames", 2, "NA", "Activo", "/Imagenes/gato4.jpg",5));
+        mascotas.put(6, new Mascota("Manolo", "Siames", 2, "NA", "Activo", "/Imagenes/gato4.jpg",6));
+        
     }
 
     public Mascota findById(Integer id){
