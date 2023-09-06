d6e1b3e969b0b0082bb21cd136e543ba98756a7f
clarck2547 <juandavidh76@gmail.com>
2023-09-06 18:56:46 -0500

    Creacion de todo el MVC
    se crea todo el MVC de veterinario, tratamientos y droga

---
+++ veterinaria/src/main/java/proyecto/web/veterinaria/repository/VeterinarioRepository.java
@@ -0,0 +1,9 @@
+package proyecto.web.veterinaria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import proyecto.web.veterinaria.entity.Veterinario;
+
+public interface VeterinarioRepository extends JpaRepository<Veterinario, Long> {
+    
+}
